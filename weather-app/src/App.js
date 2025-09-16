ea98ae93e085542f73d2293393cf36c5d97926e7
Avishka Medagamagodage <aimedagamagodage2003@gmail.com>
2025-09-16 10:34:11 +0530

    add some css to the application

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"; 
+import axios from "axios";
 
 function App() {
+
+  // const url = 'https://api.openweathermap.org/data/2.5/weather?q=Colombo,LK&appid=5c7e4512dbda754b125f83b6d898cb37';
+
   return (
     <div className="App">
-      
-    </div>
+      <div className="container">
+        <div className="top">
+          <div className="lacation">
+            <p>Sri Lanka</p>
+          </div>
+          <div className="temp">
+            <h1>30 Celcius</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+          <div className="bottom">
+            <div className="feels">
+              <p>32 Celcius</p>
+            </div>
+            <div className="humidity">
+              <p>20 %</p>
+            </div>
+            <div className="wind">
+              12 MPH
+            </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
